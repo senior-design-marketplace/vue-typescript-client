89c016d35b524f9581e93356b997a8b8fad56ddc
zildj <zildj100@gmail.com>
2019-10-15 01:10:35 -0400

    connect to backend and start sorting feature

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <template>
   <v-app>
     <div id="app">
-      <Header></Header>
-      <CardDeck></CardDeck>
+      <Header />
+      <SortBar @order="updateOrder" @sort="sortbyUpdate" />
+      <CardDeck :orderUpdate="orderData" :sortbyUpdate="sortbyData" />
     </div>
   </v-app>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import CardDeck from './components/CardDeck.vue';
+import Header from "./components/Header.vue";
+import CardDeck from "./components/CardDeck.vue";
+import SortBar from "./components/SortBar.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Header,
     CardDeck,
+    SortBar
   },
+  data: function() {
+    return {
+      orderData: "",
+      sortbyData: ""
+    };
+  },
+  methods: {
+    updateOrder(variable) {
+      this.orderData = variable;
+    },
+    sortbyUpdate(variable) {
+      this.sortbyData = variable;
+    }
+  }
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Montserrat:100');
+@import url("https://fonts.googleapis.com/css?family=Montserrat:100");
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
