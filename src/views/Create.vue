5ed9ab89a57cfa8922d0ae2597ae833b7a9ed0b0
Ruthy Levi <rlevi@stevens.edu>
2019-11-02 00:50:19 -0400

    Working on create page functionality

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,5 +1,168 @@
 <template>
-  <div>
-    <h1>This is a create page</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="8">
+        <h1>Create Project</h1>
+        <br>
+        <div>
+          <v-file-input
+            :rules="rules"
+            accept="image/png, image/jpeg, image/bmp"
+            placeholder="Pick an avatar"
+            prepend-icon="mdi-camera"
+            label="Avatar"
+          ></v-file-input>
+          <v-col cols="12" sm="6" md="12">
+            <v-text-field label="Project Title"></v-text-field>
+          </v-col>
+          <v-image-input v-model="imageData" :image-quality="0.85" clearable image-format="jpeg"/>
+          <v-toolbar>
+            <v-img v-if="avatar != null" :src="avatar" max-height="50" max-width="50"></v-img>&nbsp;&nbsp;&nbsp;&nbsp;
+            <v-toolbar-title class="headline" v-text="title"/>
+          </v-toolbar>
+          <p/>
+          <v-card>
+            <v-img :src="coverImg" contain max-height="350"></v-img>
+          </v-card>
+          <p/>
+          <v-card>
+            <v-container>
+              <v-list-item-title class="headline text-left">Description:</v-list-item-title>
+              <v-list-item class="text-left" v-text="description"></v-list-item>
+              <v-list-item-title class="headline text-left">Majors:</v-list-item-title>
+              <v-chip-group column>
+                <v-chip label v-for="major in majors" :key="major" class="noClick">{{major}}</v-chip>
+              </v-chip-group>
+              <v-list-item-title class="headline text-left">Tags:</v-list-item-title>
+              <v-chip-group column>
+                <v-chip label v-for="tag in tags" :key="tag" class="noClick">{{tag}}</v-chip>
+              </v-chip-group>
+            </v-container>
+          </v-card>
+        </div>
+        <p/>
+        <ProjectBoard v-bind:boardItems="boardItems"/>
+      </v-col>
+      <v-col cols="12" sm="4">
+        <v-card>
+          <v-btn outlined block>
+            <strong>Submit</strong>
+          </v-btn>
+        </v-card>
+        <p/>
+        <ContactInfo v-bind:members="items.contributors" v-bind:advisors="items.advisors"/>
+        <p/>
+        <Comments v-bind:comments="comments"/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import ProjectBoard from "@/components/ProjectBoard.vue";
+import ProjectMain from "@/components/ProjectMain.vue";
+import ContactInfo from "@/components/ContactInfo.vue";
+import Comments from "@/components/Comments.vue";
+import axios from "axios";
+import VImageInput from "vuetify-image-input/a-la-carte";
+
+export default {
+  components: {
+    ProjectMain,
+    ProjectBoard,
+    ContactInfo,
+    Comments,
+    VImageInput
+  },
+  data() {
+    return {
+      rules: [
+        value =>
+          !value ||
+          value.size < 2000000 ||
+          "Avatar size should be less than 2 MB!"
+      ],
+      items: [],
+      thumbnail_link:
+        "https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg",
+      cover_link:
+        "https://cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg",
+      comments: [
+        {
+          author: "Julia Cahn",
+          text: "Example question here?",
+          datetime: "Nov 13, 2019 12:00 PM"
+        },
+        {
+          author: "Ruthy Levi",
+          text: "Example response here!",
+          datetime: "Nov 13, 2019 1:00 PM"
+        },
+        {
+          author: "Herb Zieger",
+          text: "Wow this demo looks awesome!",
+          datetime: "Nov 13, 2019 12:00 PM"
+        },
+        {
+          author: "Mark Freeman",
+          text: "Ooga Booga.",
+          datetime: "Nov 13, 2019 12:00 PM"
+        },
+        {
+          author: "Jason Chlus",
+          text:
+            "Yo guys! Make sure to check out my vlog channel! https://www.youtube.com/channel/UC5UiolKpoLn920GnwidWQDQ",
+          datetime: "Nov 13, 2019 12:00 PM"
+        },
+        {
+          author: "Jakub Kolasinski",
+          text: "A bad day on the slopes beats a good day at work!",
+          datetime: "Nov 13, 2019 12:00 PM"
+        },
+        {
+          author: "Tyler Lowe",
+          text: "Have any of you guys seen Jarvis?",
+          datetime: "Nov 13, 2019 12:00 PM"
+        }
+      ],
+      boardItems: [
+        {
+          date: "November 2019",
+          desc: "Actually gettting the project board done",
+          img: []
+        },
+        {
+          date: "October 2019",
+          desc:
+            "Simple UI generation from client in response to a live call from the server. Initial UI mockups for web client finished and handed over to SDEs.",
+          img: []
+        },
+        {
+          date: "September 2019",
+          desc:
+            "Schemas validated and initial routes stubbed for future implementation. Requirements validation with EMs and generation of an ordered product backlog. Developer middlewares and other tooling written to facilitate further development. Server capable of being stood up and responding to simple requests with valid data fetched from the cloud."
+        }
+      ]
+    };
+  },
+  mounted() {
+    this.getProjectData();
+  },
+  methods: {
+    getProjectData() {
+      var url =
+        "https://3q6zl3xokg.execute-api.us-east-1.amazonaws.com/staging/projects/" +
+        this.$route.params.id;
+      axios
+        .get(url)
+        .then(response => {
+          console.log(response.data);
+          this.items = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
