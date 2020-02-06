8590c7fac1bd03f419d77c4005a9c0b047908ac7
Ruthy Levi <rlevi@stevens.edu>
2020-02-06 14:53:28 -0500

    Working on other applications table possibility

--- src/components/ApplicationList.vue
+++ src/components/ApplicationList.vue
@@ -1,93 +1,111 @@
 <template>
+<div id="app">
+  <v-app>
 <v-container>
-  <v-data-table
-    :headers="headers"
-    :items="projects"
-    :expanded="expanded"
-    item-key="name"
-    show-expand
-    class="elevation-1"
-    @click:row="clicked"
-  >
-    <template v-slot:expanded-item="{ headers }">
-      <td :colspan="headers.length">
-      <v-data-table
-  :headers="peopleHeaders"
-  :items="people"
-  item-key='name'
-  hide-default-footer
-  class='elevation-1'>
-  <template slot="item" slot-scope="props">
-        <tr>
-          <td>{{props.item.name}}</td>
-          <td>{{props.item.major}}</td>
-          <td>{{props.item.preference}}</td>
-          <td>
-          <v-btn color='success'>Accept</v-btn>
-          <v-btn color='error'>Reject</v-btn>
-          </td>
-        </tr>
-      </template>
-  </v-data-table>
-  </td>
-    </template>
-  </v-data-table>
+ <v-btn color="primary" small @click="collapseAll">Collapse All</v-btn>
+          <v-btn color="secondary" small @click="expandAll">Expand All</v-btn>
+
+          <v-data-table ref="expandableTable"
+          :headers="headers"
+          :items="desserts" hide-actions item-key="name" group-key="category">
+
+            <template slot="group" slot-scope="props">
+              <span class="font-weight-bold">
+                Group {{props.groupIndex + 1}} - {{props.groupName}}
+              </span>
+            </template>
+
+            <template slot="items" slot-scope="props">
+              <tr>
+                <td>{{ props.item.name }}</td>
+                <td>{{ props.item.calories }}</td>
+                <td>{{ props.item.fat }}</td>
+                <td>{{ props.item.carbs }}</td>
+                <td>{{ props.item.protein }}</td>
+                <td>{{ props.item.iron }}</td>
+              </tr>
+            </template>
+          </v-data-table>
 </v-container>
-</template>
+  </v-app>
+</div>
+          </template>
 
 <script>
 export default {
-  data() {
-    return {
-      expanded: [],
-      headers: [
-        {
-          text: 'Name',
-          align: 'left',
-          sortable: true,
-          value: 'name',
-        },
-        { text: 'Status', value: 'Status' },
-      ],
-      projects: [
-        {
-          name: '#this is the project they applied to',
-          Status: 'Status: Pending',
-        },
-      ],
-      peopleHeaders: [
-        {
-          text: 'Name',
-          align: 'left',
-          sortable: true,
-          value: 'name',
-        },
-        {
-          text: 'Major',
-          align: 'left',
-          sortable: true,
-          value: 'major',
-        },
-        {
-          text: 'Preference',
-          align: 'left',
-          sortable: true,
-          value: 'preference',
-        },
-      ],
-      people: [
-        {
-          name: 'BORB',
-          major: 'SWE',
-          preference: 'idk',
-        },
-      ],
-    };
-  },
+
   methods: {
-    clicked(value) {
-      this.expanded.push(value);
+    collapseAll() {
+      this.$refs.expandableTable.collapseAll();
+    },
+    expandAll() {
+      this.$refs.expandableTable.expandAll();
     },
   },
+  data: () => ({
+    headers: [
+      { text: 'Dessert (100g serving)', value: 'name' },
+      { text: 'Calories', value: 'calories' },
+      { text: 'Fat (g)', value: 'fat' },
+      { text: 'Carbs (g)', value: 'carbs' },
+      { text: 'Protein (g)', value: 'protein' },
+      { text: 'Iron (%)', value: 'iron' },
+    ],
+    desserts: [
+      {
+        value: false,
+        name: 'Orange Juice',
+        category: 'Beverage',
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0,
+        iron: '7%',
+      },
+      {
+        value: false,
+        name: 'Larabar',
+        category: 'Snack',
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5,
+        iron: '45%',
+      },
+      {
+        value: false,
+        name: 'Donut',
+        category: 'Breakfast',
+        calories: 452,
+        fat: 25.0,
+        carbs: 51,
+        protein: 4.9,
+        iron: '22%',
+      },
+
+      {
+        value: false,
+        name: 'Bagel',
+        category: 'Breakfast',
+        calories: 999,
+        fat: 28.0,
+        carbs: 151,
+        protein: 2.9,
+        iron: '29%',
+      },
+      {
+        value: false,
+        name: 'KitKat',
+        category: 'Snack',
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7,
+        iron: '6%',
+      },
+    ],
+    groupSortDescending: false,
+  }),
 };
+
 </script>
