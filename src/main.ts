e68b951b06c27591dc772229490b820697ea5127
zildj <zildj100@gmail.com>
2020-02-12 00:23:41 -0500

    Vuex for state management!

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import Vue from 'vue';
+import attachCognitoModule from '@vuetify/vuex-cognito-module';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
+
+attachCognitoModule(store, {
+  userPoolId: 'your-data-here',
+  identityPoolId: '', // This field is now optional
+  userPoolWebClientId: 'your-data-here',
+  region: 'your-data-here',
+}, 'cognito');
 
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
