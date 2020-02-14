490ed626ac02111e2826aa6d0aae0512a5b52d91
zildj <zildj100@gmail.com>
2020-02-13 20:01:24 -0500

    Clickable cards with hover

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,41 +1,41 @@
 <template :id="id">
-  <v-card min-width="350" max-width="350" min-height="350" class="mx-auto">
-    <v-container class="pa-1">
-      <v-item-group>
-        <v-list-item>
-          <v-list-item-avatar tile>
-            <v-img v-if="avatar != null" :src="avatar"></v-img>
-            <span v-else-if="title!=null"
-              class="black--text headline"
-              v-text="title.substring(0,1)"/>
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-flex class="headline" v-text="title"></v-flex>
-          </v-list-item-content>
-          <v-item v-slot:default="{ active, toggle }">
-            <v-btn icon @click="toggle">
-              <v-icon v-bind:color="active ? 'yellow accent-4' : 'gray'">mdi-star</v-icon>
-            </v-btn>
-          </v-item>
-        </v-list-item>
-        <v-divider></v-divider>
-        <v-img v-if="coverImg != null" :src="coverImg" height="194"></v-img>
-        <v-card-text>
-          <div v-if="tagline != null">
-            <div v-text="tagline"></div>
-          </div>
-          <v-chip-group column>
-            <v-chip label v-for="tag in tags" :key="tag" class="noClick">{{tag}}</v-chip>
-          </v-chip-group>
-        </v-card-text>
-        <v-row justify="end">
-          <v-card-actions>
-              <v-btn :to=/project/+id color="primary">Read More</v-btn>
-          </v-card-actions>
-        </v-row>
-      </v-item-group>
-    </v-container>
-  </v-card>
+  <v-hover v-slot:default="{ hover }">
+    <v-card min-width="350" max-width="350" min-height="350" class="mx-auto" :to=/project/+id
+    :elevation="hover ? 12 : 2">
+      <v-container class="pa-1">
+        <v-item-group>
+          <v-list-item>
+            <v-list-item-avatar tile color='primary'>
+              <v-img v-if="avatar != null" :src="avatar"></v-img>
+              <span
+                v-else-if="title != null"
+                class="black--text headline"
+                v-text="title.substring(0, 1)"
+              />
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-flex class="headline" v-text="title"></v-flex>
+            </v-list-item-content>
+            <!-- <v-item v-slot:default="{ active, toggle }">
+                <v-btn icon @click="toggle">
+                  <v-icon v-bind:color="active ? 'yellow accent-4' : 'gray'">mdi-star</v-icon>
+                </v-btn>
+              </v-item> -->
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-img v-if="coverImg != null" :src="coverImg" height="194"></v-img>
+          <v-card-text>
+            <div v-if="tagline != null">
+              <div v-text="tagline"></div>
+            </div>
+            <v-chip-group column>
+              <v-chip label v-for="tag in tags" :key="tag" class="noClick">{{ tag }}</v-chip>
+            </v-chip-group>
+          </v-card-text>
+        </v-item-group>
+      </v-container>
+    </v-card>
+  </v-hover>
 </template>
 
 <script>
