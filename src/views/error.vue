7fe6b4d2a8b06e93ec8f1939a53321cf4dea9b84
zildj <zildj100@gmail.com>
2020-03-31 18:54:06 -0400

    Remove undefined error messages

--- src/views/error.vue
+++ src/views/error.vue
@@ -2,9 +2,11 @@
   <v-container>
     <h1>
       <v-img contain max-height="500" :src="require('@/../public/assets/500.svg')" />
-      {{ $route.params.type }} Error:
+      {{ $route.params.type }} Error
       <br />
-      {{ $route.params.text }}
+      <span v-if="$route.params.text !== 'undefined'">
+        {{ $route.params.text }}
+      </span>
     </h1>
   </v-container>
 </template>
