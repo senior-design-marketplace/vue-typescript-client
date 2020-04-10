57438b0975166aabf4938869a41eab9e6aaed499
zildj <zildj100@gmail.com>
2020-04-09 21:07:42 -0400

    cards expand on hover

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,81 +1,90 @@
 <template :id="id">
   <v-hover v-slot:default="{ hover }">
-    <v-card width="350" height="350" class="mx-auto" :elevation="hover ? 12 : 2" :ripple="false">
-      <v-container class="pa-1" @click="$router.push(`/project/${id}`)" style="cursor: pointer;">
-        <v-item-group>
-          <v-list-item>
-            <v-avatar v-if="avatar != null" size="35">
-              <v-img :src="avatar" />
-            </v-avatar>
-            <v-avatar v-else-if="title != null" size="35" color="primary">
-              <span class="white--text headline" v-text="title.substring(0, 1).toLowerCase()" />
-            </v-avatar>
+    <div style="width: 350px; height: 350px" class="mx-auto">
+      <v-card
+        width="350"
+        min-height="350"
+        :max-height="hover ? '' : '350'"
+        :elevation="hover ? 12 : 2"
+        :ripple="false"
+        :style="hover ? 'z-index: 2; position: absolute;' : 'z-index: 1'"
+      >
+        <v-container class="pa-1" @click="$router.push(`/project/${id}`)" style="cursor: pointer;">
+          <v-item-group>
+            <v-list-item>
+              <v-avatar v-if="avatar != null" size="35">
+                <v-img :src="avatar" />
+              </v-avatar>
+              <v-avatar v-else-if="title != null" size="35" color="primary">
+                <span class="white--text headline" v-text="title.substring(0, 1).toLowerCase()" />
+              </v-avatar>
 
-            <v-list-item-content>
-              <v-flex class="headline" v-text="title"></v-flex>
-            </v-list-item-content>
-            <v-item v-if="onProject">
-              <v-tooltip top max-width="175">
-                <template v-slot:activator="{ on }">
-                  <span icon v-on="on">
-                    <v-icon color="primary">mdi-account-check</v-icon>
-                  </span>
-                </template>
-                <span>You are a member of this project.</span>
-              </v-tooltip>
-            </v-item>
-            <v-item v-else-if="alreadyApplied">
-              <v-tooltip top max-width="175">
-                <template v-slot:activator="{ on }">
-                  <span icon v-on="on">
-                    <v-icon color="warning">mdi-sticker-minus-outline</v-icon>
-                  </span>
-                </template>
-                <span>You have a pending application to this project.</span>
-              </v-tooltip>
-            </v-item>
-            <v-item v-else>
-              <v-tooltip top max-width="175">
-                <template v-slot:activator="{ on }">
-                  <span icon v-on="on">
-                    <v-icon v-bind:color="acceptingApps ? 'success' : 'error'">
-                      {{
-                        acceptingApps ? "mdi-sticker-check-outline" : "mdi-sticker-remove-outline"
-                      }}
-                    </v-icon>
-                  </span>
-                </template>
-                <span v-if="acceptingApps">This project is accepting applications.</span>
-                <span v-else>This project is not accepting applications.</span>
-              </v-tooltip>
-            </v-item>
-          </v-list-item>
-          <v-divider></v-divider>
-          <v-img v-if="coverImg != null" :src="coverImg" height="200"></v-img>
-          <v-img
-            v-else
-            :src="randomCover"
-            height="200"
-            gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
-          >
-            <v-overlay absolute>
-              <v-container class="white--text display-1" v-text="title" />
-            </v-overlay>
-          </v-img>
-          <v-card-text>
-            <div v-if="tagline != null" v-text="tagline" class="line-clamp" />
-            <!-- <v-chip-group column>
-              <v-chip label v-for="tag in tags" :key="tag" style="pointer-events: none;">
-                {{ tag }}
-              </v-chip>
-            </v-chip-group> -->
-          </v-card-text>
-        </v-item-group>
-      </v-container>
-      <v-btn icon absolute color="white" style="bottom: 0px; right: 0px;" @click="toggleStarred">
-        <v-icon :color="starred ? 'yellow accent-4' : 'grey'">mdi-star</v-icon>
-      </v-btn>
-    </v-card>
+              <v-list-item-content>
+                <v-flex class="headline" v-text="title"></v-flex>
+              </v-list-item-content>
+              <v-item v-if="onProject">
+                <v-tooltip top max-width="175">
+                  <template v-slot:activator="{ on }">
+                    <span icon v-on="on">
+                      <v-icon color="primary">mdi-account-check</v-icon>
+                    </span>
+                  </template>
+                  <span>You are a member of this project.</span>
+                </v-tooltip>
+              </v-item>
+              <v-item v-else-if="alreadyApplied">
+                <v-tooltip top max-width="175">
+                  <template v-slot:activator="{ on }">
+                    <span icon v-on="on">
+                      <v-icon color="warning">mdi-sticker-minus-outline</v-icon>
+                    </span>
+                  </template>
+                  <span>You have a pending application to this project.</span>
+                </v-tooltip>
+              </v-item>
+              <v-item v-else>
+                <v-tooltip top max-width="175">
+                  <template v-slot:activator="{ on }">
+                    <span icon v-on="on">
+                      <v-icon v-bind:color="acceptingApps ? 'success' : 'error'">
+                        {{
+                          acceptingApps ? "mdi-sticker-check-outline" : "mdi-sticker-remove-outline"
+                        }}
+                      </v-icon>
+                    </span>
+                  </template>
+                  <span v-if="acceptingApps">This project is accepting applications.</span>
+                  <span v-else>This project is not accepting applications.</span>
+                </v-tooltip>
+              </v-item>
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-img v-if="coverImg != null" :src="coverImg" height="200"></v-img>
+            <v-img
+              v-else
+              :src="randomCover"
+              height="200"
+              gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+            >
+              <v-overlay absolute>
+                <v-container class="white--text display-1" v-text="title" />
+              </v-overlay>
+            </v-img>
+            <v-card-text>
+              <div v-if="tagline != null" v-text="tagline" :class="hover ? '' : 'line-clamp'" />
+              <v-chip-group v-if="hover" column>
+                <v-chip label v-for="tag in tags" :key="tag" style="pointer-events: none;">
+                  {{ tag }}
+                </v-chip>
+              </v-chip-group>
+            </v-card-text>
+          </v-item-group>
+        </v-container>
+        <v-btn icon absolute color="white" style="bottom: 0px; right: 0px;" @click="toggleStarred">
+          <v-icon :color="starred ? 'yellow accent-4' : 'grey'">mdi-star</v-icon>
+        </v-btn>
+      </v-card>
+    </div>
   </v-hover>
 </template>
 
